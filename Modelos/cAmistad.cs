51301c38fa4c5a3710a6b31e701cd30eb55c7610
Neysa D. <neysadayana89@gmail.com>
2026-07-01 23:38:27 -0500

    comit, solicitud y amistad implementacion

---
+++ Modelos/cAmistad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSocialGB.Modelos
+{
+    public class cAmistad
+    {
+        #region ---Atributos---
+        private cUsuario Usuario1;
+        private cUsuario Usuario2;
+        private DateTime fecha;
+        #endregion
+    }
+}
