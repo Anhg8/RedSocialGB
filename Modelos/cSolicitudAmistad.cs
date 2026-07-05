57c2cec65d12393e1cf517a448b8801e933c97d5
jhairqh <quispejhair17@gmail.com>
2026-07-04 21:57:14 -0500

    Commit modifique cSolicitudAmistad

--- Modelos/cSolicitudAmistad.cs
+++ Modelos/cSolicitudAmistad.cs
@@ -1,89 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RedSocialGB.Modelos;
+using System;
 
-namespace RedSocialGB.Modelos
+public class cSolicitudAmistad
 {
-    public class cSolicitudAmistad
+    #region --- Atributos ---
+
+    private cUsuario aRemitente;
+    private cUsuario aDestinatario;
+    private DateTime aFechaEnvio;
+
+    #endregion
+
+    #region --- Constructores ---
+
+    public cSolicitudAmistad()
     {
-        #region --- Atributos ---
-
-        private cUsuario aRemitente;
-        private cUsuario aDestinatario;
-        private DateTime aFechaEnvio;
-        private bool aPendiente;
-
-        #endregion
-
-        #region --- Constructores ---
-
-        public cSolicitudAmistad()
-        {
-            aPendiente = true;
-        }
-
-        public cSolicitudAmistad(cUsuario pRemitente, cUsuario pDestinatario)
-        {
-            aRemitente = pRemitente;
-            aDestinatario = pDestinatario;
-            aFechaEnvio = DateTime.Now;
-            aPendiente = true;
-        }
-
-        #endregion
-
-        #region --- Propiedades ---
-
-        public cUsuario Remitente
-        {
-            get { return aRemitente; }
-            set { aRemitente = value; }
-        }
-
-        public cUsuario Destinatario
-        {
-            get { return aDestinatario; }
-            set { aDestinatario = value; }
-        }
-
-        public DateTime FechaEnvio
-        {
-            get { return aFechaEnvio; }
-            set { aFechaEnvio = value; }
-        }
-
-        public bool Pendiente
-        {
-            get { return aPendiente; }
-            set { aPendiente = value; }
-        }
-
-        #endregion
-
-        #region --- Métodos ---
-
-        public void Aceptar()
-        {
-            aPendiente = false;
-        }
-
-        public void Rechazar()
-        {
-            aPendiente = false;
-        }
-
-        public bool InvolucraA(cUsuario pUsuario)
-        {
-            return aRemitente == pUsuario || aDestinatario == pUsuario;
-        }
-
-        public override string ToString()
-        {
-            return $"{aRemitente.Nombres} -> {aDestinatario.Nombres}";
-        }
-
-        #endregion
     }
+
+    public cSolicitudAmistad(cUsuario pRemitente, cUsuario pDestinatario)
+    {
+        aRemitente = pRemitente;
+        aDestinatario = pDestinatario;
+        aFechaEnvio = DateTime.Now;
+    }
+
+    #endregion
+
+    #region --- Propiedades ---
+
+    public cUsuario Remitente
+    {
+        get { return aRemitente; }
+        set { aRemitente = value; }
+    }
+
+    public cUsuario Destinatario
+    {
+        get { return aDestinatario; }
+        set { aDestinatario = value; }
+    }
+
+    public DateTime FechaEnvio
+    {
+        get { return aFechaEnvio; }
+        set { aFechaEnvio = value; }
+    }
+
+    #endregion
+
+    #region --- Métodos ---
+
+    public bool EsRemitente(cUsuario pUsuario)
+    {
+        return aRemitente == pUsuario;
+    }
+
+    public bool EsDestinatario(cUsuario pUsuario)
+    {
+        return aDestinatario == pUsuario;
+    }
+
+    public bool InvolucraA(cUsuario pUsuario)
+    {
+        return aRemitente == pUsuario || aDestinatario == pUsuario;
+    }
+
+    public override string ToString()
+    {
+        return $"{aRemitente.Nombre} -> {aDestinatario.Nombre} ({aFechaEnvio:dd/MM/yyyy HH:mm})";
+    }
+
+    #endregion
 }
