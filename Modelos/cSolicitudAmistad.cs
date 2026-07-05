8714a8ab521315f4da5e886de570daa72d959c88
jhairqh <quispejhair17@gmail.com>
2026-07-04 21:53:18 -0500

    Commit implemente cSolicitudAmistad

--- Modelos/cSolicitudAmistad.cs
+++ Modelos/cSolicitudAmistad.cs
@@ -8,19 +8,82 @@ namespace RedSocialGB.Modelos
 {
     public class cSolicitudAmistad
     {
-        #region
-        private string id_solicitud;
-        private cUsuario emisor;
-        private cUsuario receptor;
-        private DateTime fecha_solicitud;
-        private DateTime fecha_respuesta;
+        #region --- Atributos ---
+
+        private cUsuario aRemitente;
+        private cUsuario aDestinatario;
+        private DateTime aFechaEnvio;
+        private bool aPendiente;
+
         #endregion
+
+        #region --- Constructores ---
+
         public cSolicitudAmistad()
         {
-            id_solicitud = "";
-            emisor = null;
-            receptor = null;
+            aPendiente = true;
         }
 
+        public cSolicitudAmistad(cUsuario pRemitente, cUsuario pDestinatario)
+        {
+            aRemitente = pRemitente;
+            aDestinatario = pDestinatario;
+            aFechaEnvio = DateTime.Now;
+            aPendiente = true;
+        }
+
+        #endregion
+
+        #region --- Propiedades ---
+
+        public cUsuario Remitente
+        {
+            get { return aRemitente; }
+            set { aRemitente = value; }
+        }
+
+        public cUsuario Destinatario
+        {
+            get { return aDestinatario; }
+            set { aDestinatario = value; }
+        }
+
+        public DateTime FechaEnvio
+        {
+            get { return aFechaEnvio; }
+            set { aFechaEnvio = value; }
+        }
+
+        public bool Pendiente
+        {
+            get { return aPendiente; }
+            set { aPendiente = value; }
+        }
+
+        #endregion
+
+        #region --- Métodos ---
+
+        public void Aceptar()
+        {
+            aPendiente = false;
+        }
+
+        public void Rechazar()
+        {
+            aPendiente = false;
+        }
+
+        public bool InvolucraA(cUsuario pUsuario)
+        {
+            return aRemitente == pUsuario || aDestinatario == pUsuario;
+        }
+
+        public override string ToString()
+        {
+            return $"{aRemitente.Nombres} -> {aDestinatario.Nombres}";
+        }
+
+        #endregion
     }
 }
